e240a7de6019df4440e67890067f772277fbaa23
unknown <tjhorne188@gmail.com>
2020-07-24 15:22:19 -0700

    more tuning

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -19,64 +19,94 @@ namespace ProjectTemplate
         ////////////////////////////////////////////////////////////////////////
         ///replace the values of these variables with your database credentials
         ////////////////////////////////////////////////////////////////////////
-        private string dbID = "cis440template";
-        private string dbPass = "!!Cis440";
-        private string dbName = "cis440template";
+        private string dbID = "summer2020group1";
+        private string dbPass = "!!Group1";
+        private string dbName = "summer2020group1";
         ////////////////////////////////////////////////////////////////////////
 
         ////////////////////////////////////////////////////////////////////////
         ///call this method anywhere that you need the connection string!
         ////////////////////////////////////////////////////////////////////////
-        private string getConString() {
+        private string getConString()
+        {
             return "SERVER=107.180.1.16; PORT=3306; DATABASE=" + dbName + "; UID=" + dbID + "; PASSWORD=" + dbPass;
         }
         ////////////////////////////////////////////////////////////////////////
 
-
+    
 
         /////////////////////////////////////////////////////////////////////////
         //don't forget to include this decoration above each method that you want
         //to be exposed as a web service!
+
         [WebMethod(EnableSession = true)]
         /////////////////////////////////////////////////////////////////////////
-        public string TestConnection()
+        public string[][] TestRetrieval()
+        { 
+            string[][] data;
+            try
+			{
+				string testQuery = "select * from Scenarios where scenarioID='S0002'";
+
+				////////////////////////////////////////////////////////////////////////
+				///here's an example of using the getConString method!
+				////////////////////////////////////////////////////////////////////////
+				MySqlConnection con = new MySqlConnection(getConString());
+                ////////////////////////////////////////////////////////////////////////
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(testQuery, con);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                int count = 0;
+                while (rdr.Read())
+                {
+                    count++;
+                }
+                rdr.Close();
+                rdr = cmd.ExecuteReader();
+                data = new string[count][];
+                int fieldAmount = rdr.FieldCount;
+                for(int j =0; j<count; j++)
+                {
+                    rdr.Read();
+                    data[j] = new string[fieldAmount];
+                    for(int i = 0; i < fieldAmount; i++)
+                    {
+                        data[j][i] = Convert.ToString(rdr.GetValue(i));
+                    }
+                }
+                rdr.Close();
+                con.Close();
+                return data;
+            }
+			catch (Exception e)
+			{
+                string[][] jaggedArray = new string[3][];
+                jaggedArray[0] = new string[] { e.Message };
+                return jaggedArray;
+            }
+            
+        }
+        [WebMethod(EnableSession = true)]
+        /////////////////////////////////////////////////////////////////////////
+        public string TestEditing()
         {
+            
             try
             {
-                string testQuery = "select * from test";
-
-                ////////////////////////////////////////////////////////////////////////
-                ///here's an example of using the getConString method!
-                ////////////////////////////////////////////////////////////////////////
+                string testQuery = "UPDATE `summer2020group1`.`Scenarios` SET `scenarioText` = '1st test', `scenarioHeading` = '1st heading' WHERE (`scenarioID` = 's0001');";
                 MySqlConnection con = new MySqlConnection(getConString());
-                ////////////////////////////////////////////////////////////////////////
-
+                
+                con.Open();
                 MySqlCommand cmd = new MySqlCommand(testQuery, con);
-                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
-                DataTable table = new DataTable();
-                adapter.Fill(table);
-                return "Success!";
+                cmd.ExecuteNonQuery();
+
+                return "Edit Successful";
             }
             catch (Exception e)
             {
-                return "Something went wrong, please check your credentials and db name and try again.  Error: " + e.Message;
+                
+                return e.Message;
             }
         }
-        [WebMethod]
-        public Person[] getMatts(int userCount)
-        {
-            List<Person> people = new List<Person>();
-            for (int i = 0; i < userCount; i++)
-            {
-                Person matt = new Person();
-                matt.firstName = "Matt";
-                matt.lastName = "Mozingo";
-                matt.phoneNumber = 1234567891;
-                people.Add(matt);
-            }
-
-            return people.ToArray();
-        }
-     
-     }
+    }
 }
