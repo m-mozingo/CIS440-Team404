05365eff77aa0c4a52b96756120b412a1e526855
Baden <baden.swallow@asu.edu>
2020-07-25 18:56:42 -0600

    Basic HTML pages implemented with CSS. WebService method created to create new User Accounts.

---
+++ ProjectTemplate/HTML_Files/JavaScript.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+function processForm(inputForm) {
+
+    if (!inputForm.checkValidity()) {
+        alert("See highlighted input boxes, there are input errors!");
+    }
+    else {
+
+        var createAccountForm = document.getElementById('createAccountFormId')
+
+        var fname = document.getElementById(fNameCreateId).value;
+
+        alert(fname);
+
+
+        //window.location.href = 'file:///C:/Users/baden/OneDrive/Desktop/CIS%20440%20Project/LoginPage/Login%20Page.html';
+
+    }
+
+}
--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -108,5 +108,54 @@ namespace ProjectTemplate
                 return e.Message;
             }
         }
+
+        //EXAMPLE OF AN INSERT QUERY WITH PARAMS PASSED IN.  BONUS GETTING THE INSERTED ID FROM THE DB!
+        [WebMethod]
+        public void CreateAccount(string FirstName, string LastName, string Email, string Username, string Password, string Department)
+        {
+            //string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+            //the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
+            //does is tell mySql server to return the primary key of the last inserted row.
+            string sqlQuery = "INSERT INTO `summer2020group1`.`UserAccounts` (`FirstName`, `LastName`, `Email`, `Username`, `Password`, `Department`) " +
+                "VALUES (@FirstName, @LastName, @Email, @Username, @Password, @Department);";
+
+
+            // "INSERT into UserAccounts (FirstName, LastName, Email, Username, Password, Department) " +
+            // "values(@idValue, @passValue, @fnameValue, @lnameValue, @emailValue); SELECT LAST_INSERT_ID();";
+
+            MySqlConnection sqlConnection = new MySqlConnection(getConString());
+
+            sqlConnection.Open();
+            MySqlCommand sqlCommand = new MySqlCommand(sqlQuery, sqlConnection);
+
+            sqlCommand.Parameters.AddWithValue("@FirstName", HttpUtility.UrlDecode(FirstName));
+            sqlCommand.Parameters.AddWithValue("@LastName", HttpUtility.UrlDecode(LastName));
+            sqlCommand.Parameters.AddWithValue("@Email", HttpUtility.UrlDecode(Email));
+            sqlCommand.Parameters.AddWithValue("@Username", HttpUtility.UrlDecode(Username));
+            sqlCommand.Parameters.AddWithValue("@Password", HttpUtility.UrlDecode(Password));
+            sqlCommand.Parameters.AddWithValue("@Department", HttpUtility.UrlDecode(Department));
+
+
+            //this time, we're not using a data adapter to fill a data table.  We're just
+            //opening the connection, telling our command to "executescalar" which says basically
+            //execute the query and just hand me back the number the query returns (the ID, remember?).
+            //don't forget to close the connection!
+            
+            //we're using a try/catch so that if the query errors out we can handle it gracefully
+            //by closing the connection and moving on
+            try
+            {
+                int accountID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                //here, you could use this accountID for additional queries regarding
+                //the requested account.  Really this is just an example to show you
+                //a query where you get the primary key of the inserted row back from
+                //the database!
+            }
+            catch (Exception e)
+            {
+            }
+            sqlConnection.Close();
+        }
+
     }
 }
